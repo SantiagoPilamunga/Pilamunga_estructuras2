eb6fce7db6f8f97692acebdebbdaa4724c1087f3
Santiago <santiago.pilamunga@udla.edu.ec>
2024-01-16 14:58:11 -0500

    Inicio

---
+++ ejemplo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    struct alumno
+    {
+        int matricula;
+        char nombre[50];
+        char direccion[50];
+        char materia[50];
+        float nota;
+    };
+    return 0;
+}
